9c07a59e0042d4118eee8a9a7b5efeb7fbc7917a
malshan <www.newakshalin@gmail.com>
2025-03-31 09:14:02 +0530

    jwt hidden 😊😊

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 export function saveUser(req, res) {
     if (req.body.role == "admin") {
         if (req.user == null) {
@@ -61,7 +63,7 @@ export function loginUser(req, res) {
                     isDisabled: user.isDisable,
                     isEmailVerified: user.isEmailVarified
                 }
-                const token = jwt.sign(userData, "random456")
+                const token = jwt.sign(userData, process.env.JWT_KEY)
                 res.json({
                     message: "login successfull",
                     token: token
--- middleWare/verifyJwt.js
+++ middleWare/verifyJwt.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 export default function verifyJWT(req, res, next) {
     const header = req.header("Authorization")
     if (header != null) {
         const token = header.replace("Bearer ", "")
-        jwt.verify(token, "random456", (err, decoded) => {
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
             if (decoded != null) {
                 req.user = decoded
             }
